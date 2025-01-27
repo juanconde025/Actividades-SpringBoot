531ada611fff75063eb367e89aa2921aafab91e8
Juan David Conde Martinez <166556117+juanconde025@users.noreply.github.com>
2025-01-27 17:32:37 -0500

    Update ProductoFormController.java

--- Actividad4/src/main/java/com/c3/Actividad4/controller/ProductoFormController.java
+++ Actividad4/src/main/java/com/c3/Actividad4/controller/ProductoFormController.java
@@ -6,20 +6,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ProductoFormController {
+
+    private static final List<Producto> productos = new ArrayList<>();
+
     @GetMapping("/producto/formulario")
-    public String formulario() {
+    public String mostrarFormulario() {
         return "index";
     }
 
-    @PostMapping("producto/registrar")
-    public String registrar(@RequestParam String nombre, @RequestParam double precio, @RequestParam String descripcion, Model model) {
+    @PostMapping("/producto/registrar")
+    public String registrarProducto(
+            @RequestParam("nombre") String nombre,
+            @RequestParam("precio") double precio,
+            @RequestParam("descripcion") String descripcion,
+            Model model) {
+
         Producto producto = new Producto(nombre, precio, descripcion);
+        productos.add(producto);
 
         model.addAttribute("producto", producto);
 
         return "confirmacion";
     }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
 }
