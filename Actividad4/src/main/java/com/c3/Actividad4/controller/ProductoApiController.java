a22115682e0b0b4b2152b7405eb16f82e82aa426
Juan David Conde Martinez <166556117+juanconde025@users.noreply.github.com>
2025-01-27 17:33:02 -0500

    Update ProductoApiController.java

--- Actividad4/src/main/java/com/c3/Actividad4/controller/ProductoApiController.java
+++ Actividad4/src/main/java/com/c3/Actividad4/controller/ProductoApiController.java
@@ -11,11 +11,7 @@ import java.util.List;
 public class ProductoApiController {
     @GetMapping("/api/productos")
     public List<Producto> getProductos() {
-        List<Producto> productos = new ArrayList<>();
-        productos.add(new Producto("Gorra del verde", 20.00,"Gorra del mas grANde"));
-        productos.add(new Producto("Camisa del verde", 50.00,"Camisa Local del mas grANde"));
-        productos.add(new Producto("Zapas del verde", 100.00,"Zapas del mas grANde"));
-
-        return productos;
+        ProductoFormController productoFormController = new ProductoFormController();
+        return productoFormController.getProductos();
     }
 }
